4e746a4854a388fa2c8c17ccf0c62ef301571e2a
FemLolStudio <femlol1femlol@gmail.com>
2025-02-11 09:31:52 +0100

    initial commit

--- src/http_server/handlers/index.rs
+++ src/http_server/handlers/index.rs
@@ -2,7 +2,11 @@ use std::sync::LazyLock;
 
 use axum::response::Html;
 
-static INDEX: LazyLock<String> = LazyLock::new(|| std::fs::read_to_string("files/index.html").unwrap_or("none".to_string()));
+static INDEX: LazyLock<String> = LazyLock::new(|| {
+    std::fs::read_to_string("files/index.html")
+        .unwrap_or("none".to_string())
+        .replace("{domain}", &crate::DOMAIN)
+});
 
 pub async fn request_handler() -> Html<String> {
     Html(INDEX.to_string())
